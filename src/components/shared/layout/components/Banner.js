29b78a83f65a193db06e9dd3289a2d32ecfa39a9
Khalil Stemmler <metroidman12@gmail.com>
2019-03-30 00:07:09 -0400

    Made the banner closable.

--- src/components/shared/layout/components/Banner.js
+++ src/components/shared/layout/components/Banner.js
@@ -9,14 +9,60 @@ const messages = [
   <div className="message">what the hell</div>
 ]
 
-const Banner = ({ siteTitle }) => (
-  <header>
-    {messages[0]}
-    <div className="close-button">
-      <img alt="Close" src={closeButton}></img>
-    </div>
-  </header>
-)
+
+class Banner extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      closed: !this.shouldBannerOpen()
+    }
+
+    this.close = this.close.bind(this);
+    this.shouldBannerOpen = this.shouldBannerOpen.bind(this);
+    this.setBannerClosedExpiry = this.setBannerClosedExpiry.bind(this);
+  }
+
+  close () {
+    this.setState({ closed: true })
+    this.setBannerClosedExpiry();
+  }
+
+  shouldBannerOpen () {
+    const item = localStorage.getItem('banner-closed');
+    if (item) {
+      try {
+        const expiry = JSON.parse(item);
+        const expiryTime = new Date(expiry.timestamp);
+        const now = new Date()
+        const isStillExpired = (now.getTime() - expiryTime.getTime()) < 0;
+        return !isStillExpired;
+      } catch (err) {
+        return true;
+      }
+    } else {
+      return true;
+    }
+  }
+
+  setBannerClosedExpiry () {
+    const expiry = new Date();
+    expiry.setMinutes(expiry.getMinutes() + 20)
+    const object = { value: "banner-closed", timestamp: expiry }
+    localStorage.setItem("banner-closed", JSON.stringify(object));
+  }
+
+  render () {
+    const isBannerClosed = this.state.closed;
+    return (
+      <header className={`${isBannerClosed ? 'closed' : ''}`}>
+        {messages[0]}
+        <div onClick={this.close} className="close-button">
+          <img alt="Close" src={closeButton}></img>
+        </div>
+      </header>
+    )
+  }
+}
 
 Banner.propTypes = {
   siteTitle: PropTypes.string,
