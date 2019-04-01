f95cf5a7bb17d90c6a0734c4fb74c5a7bb20b7de
Khalil Stemmler <metroidman12@gmail.com>
2019-04-01 15:07:41 -0400

    Change founding year.

--- src/assets/prose.js
+++ src/assets/prose.js
@@ -15,7 +15,7 @@ export default {
     ),
     "description-expanded": (
       <span>
-        I co-founded <a href="https://univjobs.ca">Univjobs</a> in 2016 and learned a lot about making code scale. At some 
+        I co-founded <a href="https://univjobs.ca">Univjobs</a> in 2017 and learned a lot about making code scale. At some 
         point during <Link to="/about">my current work</Link> at <a href="https://www.dev6.com/">Aquent Dev6</a> as a consultant and 
         reading as many <Link to="/books">books</Link> on software as I could digest, I've come to realize that 
         there were a lot of pitfalls I could have avoided if I knew the patterns and principles to guide me. That's what this 
