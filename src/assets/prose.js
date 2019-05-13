f663f367ee643c1b3c62475b5bf1062233963ecc
Khalil Stemmler <metroidman12@gmail.com>
2019-05-13 18:28:40 -0400

    Get rid of DEV6 site.

--- src/assets/prose.js
+++ src/assets/prose.js
@@ -16,8 +16,7 @@ export default {
     "description-expanded": (
       <span>
         I co-founded <a href="https://univjobs.ca">Univjobs</a> in 2017 and learned a lot about making code scale. At some 
-        point during <Link to="/about">my current work</Link> at <a href="https://www.dev6.com/">Aquent Dev6</a> as a consultant and 
-        reading as many <Link to="/books">books</Link> on software as I could digest, I've come to realize that 
+        point during my work as a consultant and reading as many <Link to="/books">books</Link> on software as I could digest, I've come to realize that 
         there were a lot of pitfalls I could have avoided if I knew the patterns and principles to guide me. That's what this 
         site is about: things I think all professional software people should know.
       </span>
