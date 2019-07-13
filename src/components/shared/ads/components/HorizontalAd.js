1df36254c440321cc07eaba8d2403a6dbd7f7df0
Khalil Stemmler <metroidman12@gmail.com>
2019-07-13 12:21:59 -0400

    Remove  adsense script.

--- src/components/shared/ads/components/HorizontalAd.js
+++ src/components/shared/ads/components/HorizontalAd.js
@@ -3,11 +3,6 @@ import AdSense from 'react-adsense';
 
 const HorizonalAd = () => (
   <div style={{ width: '100%' }}>
-    <script dangerouslySetInnerHTML={{
-      __html: `(adsbygoogle = window.adsbygoogle || []).push({});`
-    }}>
-      
-    </script>
     <AdSense.Google
       client='ca-pub-5892691365556220'
       slot='5129508414'
